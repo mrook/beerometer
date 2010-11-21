083280eda9cc49d97f9c5b036aac456e113bb2ba
michiel.rook <michiel.rook@1a1b8328-249a-65ca-25b6-096b8cf93da6>
2010-11-21 20:41:15 +0000

    Move R class to lower package

--- BeerOMeter/src/nl/beer/BeerOMeter/activities/BeerListActivity.java
+++ BeerOMeter/src/nl/beer/BeerOMeter/activities/BeerListActivity.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import nl.beer.R;
+import nl.beer.BeerOMeter.R;
 import nl.beer.BeerOMeter.db.BeerStoreOpenHelper;
 import nl.beer.BeerOMeter.db.ExternalStorageHelper;
 import nl.beer.BeerOMeter.model.BeerItem;
